b03d7bd81f6d9ba18a28ca1e9bd8575d7b4b933d
je-martini <jesusmartini2017@gmail.com>
2022-10-23 09:05:15 -0400

    adding 9 nodes with the img, tittle and price

--- jesus_app/src/index.js
+++ jesus_app/src/index.js
@@ -3,4 +3,29 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = 'https://platzi-avo.vercel.app/api/avo'
+
+window
+.fetch(url)
+.then((respuesta) => respuesta.json())
+.then((respuesta_json) => {
+    const all_items = [];
+
+    respuesta_json.data.forEach(item => {
+        // console.log(item.name)
+
+        const img = document.createElement('img');
+        
+        const title = document.createElement('h2');
+        
+        const price = document.createElement('p');
+        
+        const container = document.createElement('div')
+
+        container.append(img, title, price);
+
+        all_items.push(container)
+    });
+    document.body.append(...all_items)
+})
+
